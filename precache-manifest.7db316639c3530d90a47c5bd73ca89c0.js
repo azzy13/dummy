97816cfab06691eae70f8484127624b04b38cffe
azzy13 <azharhasan013@gmail.com>
2020-10-13 16:43:09 +0530

    Updates

---
+++ precache-manifest.7db316639c3530d90a47c5bd73ca89c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd1e132f39bff76541c2403f72b214b",
+    "url": "/dummy/index.html"
+  },
+  {
+    "revision": "334ac2e672f30f10ddc5",
+    "url": "/dummy/static/css/main.1764b4f5.chunk.css"
+  },
+  {
+    "revision": "30b63a4fc84c940cb457",
+    "url": "/dummy/static/js/2.0f8064d3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dummy/static/js/2.0f8064d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "334ac2e672f30f10ddc5",
+    "url": "/dummy/static/js/main.65efd590.chunk.js"
+  },
+  {
+    "revision": "140459fb21d08ee5968e",
+    "url": "/dummy/static/js/runtime-main.6a883823.js"
+  },
+  {
+    "revision": "29318b6c0840a3ebce4b68ca0fc6aa9d",
+    "url": "/dummy/static/media/kanki.29318b6c.jpg"
+  }
+]);
