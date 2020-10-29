904f4fcd969bc9322eb411a6810013c03cb1fadb
azzy13 <azharhasan013@gmail.com>
2020-10-29 09:19:36 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+import About from './components/About';
+import Header from './components/Header';
+import Home from './components/Home';
+
+const App = () => {
   return (
     <Router>
-      <>
+      <Fragment>
         <Header />
         <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-      </>
+        <Route
+          exact
+          path='/about'
+          render={(props) => (
+            <About {...props} name={'Azhar Hasan'} rgno={'18113174'} />
+          )}
+        />
+      </Fragment>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const About = () => {
+const About = ({ name, rgno }) => {
   return (
     <div>
-      <h1>Simple React-strap card page</h1>
+      <h1 style={{ backgroundColor: 'grey', textAlign: 'center' }}>
+        Welcome {name}, Register No. {rgno}
+      </h1>
+      <form style={{ textAlign: 'center' }}>
+        <input type='text' placeholder='Department' />
+        <br />
+        <input type='text' placeholder='Branch' />
+        <br />
+        <input type='int' placeholder='Year of joining' />
+        <br />
+        <input type='text' placeholder='Year of completion' />
+        <br />
+        <input type='submit' value='Submit' />
+      </form>
     </div>
   );
 };
